661ac06d123d9ab412f14a136bd835f3e566ea75
Birgit <birgit.bartenschlager@web.de>
2023-12-19 15:39:14 +0100

    Initial commit

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button className="btn btn-accent" onClick={() => console.log("click")}>
+        Add to Cart
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
